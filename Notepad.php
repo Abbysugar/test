75fb2b8016ff1c752148ccea98b5ec4cbe562c9d
Abbysugar <bukksbisuga@gmail.com>
2017-06-03 10:24:14 -0700

    made changes to notepad

--- Notepad.php
+++ Notepad.php
@@ -6,22 +6,30 @@ session_start();
 		protected $notes;
 		public $title;
 		public $body;
-		public function __construct()
+		public function __construct($notes, $title, $body)
 		{
-			$this->notes = [];
+			$this->notes=[];
 			$this->title = $title;
 			$this->body = $body;
-			//$_SESSION['notes'] = $this->notes;
+			$_SESSION['notes'] = $this->notes;
 		}
-		public function getNote(){
+		public function setNote()
+		{
+			$this->notes = $notes;
+		}
+	
+		public function getNote()
+		{
 			return $this->notes;
 		}
+
 		public function addNote()
 		{
 			//$this->notes = $_SESSION['notes'];
-			$this->notes[$_POST[$title]] = $_POST['body'];
+			$this->notes[$_POST['title']] = $_POST['body'];
 			//$_SESSION['notes'] = $this->notes;
 		}
+		
 		public function seeAll()
 		{
 			return $this->notes;
@@ -31,47 +39,52 @@ session_start();
 		{
 			return $this->notes[$title];
 		}
+		
 		public function edit($title)
 		{
 			$this->notes[$_POST['title']] = $_POST['body'];
 		}
+		
 		public function delete($title)
 		{
 			unset($this->notes[$title]);
 		}
+		
 		public function deleteAll()
 		{
 			unset($this->notes);
 		}
 
-
 	}
 
-	if (empty($_SESSION['notebook'])){
+	if (empty($_SESSION['notebook']))
+	{
 		$_SESSION['notebook'] = new Notebook();
 	}
-	
-	if($_POST['title'] && $_POST['body']){
+		
+	if(isset($_POST['title']) && isset($_POST['body']))
+	{
 		$_SESSION['notebook']->addNote();
 	}
-	// if($_POST['title']){
+
+	
+	// if ($_POST['title'])
+	// {
 	// 	$_SESSION['notebook']->seeOne($_POST['title']);
 	// }
-
-
 ?>
 
 		<!DOCTYPE html>
 		<html>
 		<head>
-			<title>Note Book</title>
+			<title>Notebook</title>
 		</head>
 		<body>
-		<h1>My Online Notebook</h1>
+		<h1>MY NOTEBOOK</h1>
 
 			<form class="form" action="" method="post">
-				<input type="text" name="title" required><br>
-				<textarea name="body" cols="30" rows="5" required></textarea>
+				<input type="text" name="title" required><br><br><br>
+				<textarea name="body" cols="30" rows="5" required></textarea><br><br>
 				<input type="submit" value="submit">
 			</form>
 			
